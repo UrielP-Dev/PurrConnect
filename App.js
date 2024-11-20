80155172960a12fe874dc9e26584f6b9a032b361
UrielP-Dev <kaattoo9@gmail.com>
2024-11-19 23:18:10 -0600

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {Pressable, Text, View} from 'react-native';
+import './global.css'
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <LinearGradient
+            colors={['#F3C5C5', '#FFFFFF']}
+            start={{ x: 0, y: 0.7 }}
+            end={{ x: 0, y: 1 }}
+            className="justify-center items-center flex-1"
+        >
+            <Text className="text-white text-4xl font-bold mb-20">
+                PurrConnect
+            </Text>
+            <Pressable  className='justify-center items-center bg-secondary rounded-full w-2/6 h-10 '>
+                <Text className='text-black font-bold'>Login</Text>
+            </Pressable>
+            <StatusBar style="auto" />
+        </LinearGradient>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: { colors: {
+        main: '#F3C5C5',
+        secondary: '#C1A3A3',
+        tertiary: '#886F6F',
+        brownie: '#694E4E',
+            white: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
