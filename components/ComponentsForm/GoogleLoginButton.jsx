4fcd83fef9b572d9fc1841fbc13576f913d421ba
Antonio Perez <antonio.perez@jala.university>
2024-11-22 08:08:02 -0600

    Add google Auth

--- components/ComponentsForm/GoogleLoginButton.jsx
+++ components/ComponentsForm/GoogleLoginButton.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, Alert } from 'react-native';
+import { TouchableOpacity, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { auth } from '../../Utils/Firebase'; // Importa el Auth desde tu configuración centralizada
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import * as Google from 'expo-auth-session/providers/google';
+import { auth } from '../../Utils/Firebase';
 
 const GoogleLoginButton = () => {
-    const handleGoogleLogin = () => {
-        const provider = new GoogleAuthProvider();
+    const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+        androidClientId: '', // Client ID de Android
+    });
 
-        // Inicia el flujo de redirección
-        signInWithRedirect(auth, provider);
+    React.useEffect(() => {
+        if (response?.type === 'success') {
+            const { id_token } = response.params;
+
+            // Usamos el token para autenticar con Firebase
+            const credential = GoogleAuthProvider.credential(id_token);
+            signInWithCredential(auth, credential)
+                .then(() => {
+                    console.log('Usuario autenticado exitosamente con Google');
+                })
+                .catch((error) => {
+                    console.error('Error al autenticar con Google: ', error);
+                });
+        }
+    }, [response]);
+
+    const handleGoogleLogin = () => {
+        promptAsync();
     };
 
     return (
         <TouchableOpacity
             onPress={handleGoogleLogin}
+            disabled={!request} // Deshabilitar si la solicitud no está lista
             className="w-full flex-row items-center justify-center bg-white py-4 rounded-xl shadow-sm"
         >
             <View className="w-5 h-5 mr-3 items-center justify-center">
