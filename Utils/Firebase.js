a8f38cb4bd55e03fff967ebba2588a0c97e408b3
UrielP-Dev <kaattoo9@gmail.com>
2024-12-02 13:38:26 -0600

    hot fixes

--- Utils/Firebase.js
+++ Utils/Firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // Importa Firebase Storage
+import { getStorage } from "firebase/storage";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';  // Importa AsyncStorage
 
 const firebaseConfig = {
 
@@ -10,8 +11,12 @@ const firebaseConfig = {
 // Verifica si ya existe una instancia
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
+// Configura la persistencia de la autenticación utilizando AsyncStorage
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
 const db = getFirestore(app);
-const auth = getAuth(app);
 const storage = getStorage(app);
 
 export { auth, db, storage };
